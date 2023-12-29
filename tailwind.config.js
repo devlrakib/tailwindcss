0b1bac65369c7957045d4b883e4ae64b1020c88b
devlrakib <devrakib5@gmail.com>
2023-12-29 11:44:23 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    // !Tailwind CSS Customize CSS System
+    screens:{
+     'sm':'400px'  
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
